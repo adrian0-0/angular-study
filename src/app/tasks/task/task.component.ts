9bb487917669e0512f58e7654a969d22c6be033b
Adrian Vinícius Roma Alves <adrianromaalves@hotmail.com>
2025-09-26 17:44:51 -0300

    feat: add users and tasks components

---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ITask } from './task.model';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css',
+})
+export class TaskComponent {
+  @Input({ required: true }) task!: ITask;
+  @Output() complete = new EventEmitter<string>();
+
+  onCompleteTask() {
+    this.complete.emit(this.task.id);
+  }
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,53 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../../assets/dummy.users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  output,
+  Output,
+} from '@angular/core';
+import { IUser } from './user.model';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]); 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  @Input({ required: true }) user!: IUser;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
 
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
 
+  // Zones
+  //  @Input({ required: true }) id!: string;
+  //  @Input({ required: true }) avatar!: string;
+  //  @Input({ required: true }) name!: string;
 
-//  get imagePath() {
-//    return 'assets/users/' + this.selectedUser.avatar
-//  }
-  
+  // Essas duas linhas abaixo são Zones e âmbas fazem a mesma coisa
+  // @Output() select = new EventEmitter<string>();
+  //  select = output<string>();
+
+  //  Signal
+  //  avatar = input.required<string>()
+  //  name = input.required<string>()
+
+  // Signal
+  //  imagePath = computed(() =>  {
+  //    return 'assets/users/' + this.avatar();
+  //  })
+  // Zones
+  //get imagePath() {
+  //  return 'assets/users/' + this.avatar;
+  // }
+
   onSelectedUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIndex])
+    this.select.emit(this.user.id);
   }
 }
